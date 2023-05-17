1c68ddf6798e172b3cc32ef40d78a93450ee70dd
Ajay Singh <contact@ajaysingh.ca>
2023-05-17 16:53:56 -0400

    Add basic styling for header component

--- src/components/Avatar.js
+++
@@ -1,7 +0,0 @@
-import "../styles/Avatar.scss";
-
-export default function Avatar(props) {
-    const { img } = props;
-    return <img src={img} alt="Avatar Img" />;
-
-}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,37 @@
-import "../styles/Header.scss";
-import Button from "../components/Button";
-import Avatar from "../components/Avatar";
+import "./Header.scss";
 
 import { ReactComponent as Logo } from "../assets/Logo/BrainFlix-logo.svg";
-import { ReactComponent as SearchIcon } from "../assets/Icons/search.svg";
-import { ReactComponent as UploadIcon } from "../assets/Icons/upload.svg";
-import AvatarIcon from "../assets/Images/Mohan-muruge.jpg";
+
+import AvatarImg from "../assets/Images/Mohan-muruge.jpg";
+
+
 
 export default function Header() {
   return (
     <>
       <div className="header-item">
-                        <div className="header-item__logo">
-                        <Logo />
-                        </div>
+        <div className="header-item__logo">
+          <Logo />
+        </div>
+        <div className="header-item__inputavatar">
+          <div className="header-item__input">
+            <input
+              type="text"
+              className="header-item__searchInput"
+              placeholder="Search"
+            />
+          </div>
 
-                    <div className="header-item__container">
-                            <div className="header-item__input">
-                                <input
-                                type="text"
-                                className="header-item__searchInput"
-                                placeholder="Search"
-                                />
-                            </div>
-                            <div className="header-item__button">
-                                <Button icon={UploadIcon} buttonText="Upload" />
-                            </div>
+          <div className="header-item__avatar">
+            <img src={AvatarImg} alt="Avatar Img" className="header-item__avatar-icon" />
+          </div>
+        </div>
 
-                            <div className="header-item__avatar">
-                                <Avatar img={AvatarIcon} />
-                            </div>
-                    </div>
+        <div className="header-item__button">
+          <button type="button">
+            <div>Upload</div>
+          </button>
+        </div>
       </div>
     </>
   );
