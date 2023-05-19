28a57ef31bde73b53194da13e3816da914f978c5
Ajay Singh <contact@ajaysingh.ca>
2023-05-18 23:32:31 -0400

    add map and filter to app.js to pull data from json and not duplicate

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import './styles/App.scss';
 import Header from './components/Header';
 import Video from './components/Video/Video';
 import VideoContent from './components/VideoContent/VideoContent';
+import VideoList from './components/VideoList/VideoList';
 
+// Grabs data from json
+import VideoData from './data/video-details.json';
+
+
+// uses state
 import { useState } from 'react';
 
 function App() {
+  // sets default state to index 0 (BMX Rampage)
+ const [videoOrder, setVideoOrder] = useState(1);
+// Turns videodata from json file into videoOrder and assigns variable
+ const videoDetails = VideoData[videoOrder];
 
-// const [videoOrder, setvideoOrder] = useState[0];
+// gets data up from VideoList (data up action down)
+const handleImageClick = () => {
+  // videoOrder
+}
+
+
+//  Fildering to not use whatever is in useState
+ const filteredVideoData = VideoData.filter((video, index) => index !== videoOrder);
+ 
+
 
   return (
     <>
@@ -19,9 +38,23 @@ function App() {
       <Video/>
     </div>
 
-<div className='video-content'>
-  <VideoContent/>
+    <div className='video-content'>
+    {/* Props to use data from json */}
+        <VideoContent
+          title={videoDetails.title}
+          channel={videoDetails.channel}
+          timestamp={videoDetails.timestamp}
+          views={videoDetails.views}
+          likes={videoDetails.likes}
+          description={videoDetails.description}
+        />
+      </div>
+
+<div className='video-List'>
+<VideoList videoData={filteredVideoData} onImageClick={handleImageClick}  />
 </div>
+
+
 
     </>
   );
---
+++ src/components/VideoList/VideoList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './VideoList.scss';
+
+
+export default function VideoList(props) {
+    // passing data up
+    const { videoData, onImageClick } = props;
+    return (
+    <section className='video container'>
+    <h3 className='video__header'>NEXT VIDEO</h3>
+
+        <ul className='video__list'>
+        {videoData.map((video) => (
+          <div className='video__item'>
+          {/* making onclick for button which calls onImageClick on props  */}
+          <div className='video__item--image' onClick={() => onImageClick()}>
+              <img className='video__item--img' src={video.image} alt='Video Image'   />
+            </div>
+
+            <div className='video__item--details'>
+              <div className='video__item--content'>
+                <p className='video__item--header'>{video.title}</p>
+                <p className='video__item--name'>{video.channel}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+
+        </ul>
+
+
+
+
+    </section>
+  )
+}
